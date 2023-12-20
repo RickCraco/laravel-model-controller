c44b63d1a27f33f2ef12484ea28a0bf952adc7b0
RickCraco <riccardocracolici43@gmail.com>
2023-12-20 16:40:24 +0100

    aggiunti elementi di stile alla home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,8 @@
     <main>
         <h1 class="text-center">Welcome</h1>
         <p class="text-center">If you want to select one of our movies please click here :</p>
-        <a href="{{ route('movies.index') }}" class="btn btn-primary"></a>
+        <div class="d-flex justify-content-center">
+            <a href="{{ route('movies.index') }}" class="btn btn-primary">Start</a>
+        </div>
     </main>
 @endsection
