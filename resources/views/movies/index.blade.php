e0167ec9dda39502e57f68930d9eb410485fc4b0
RickCraco <riccardocracolici43@gmail.com>
2023-12-20 16:52:55 +0100

    sistemati elementi di stile

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -6,16 +6,21 @@
 
 <h1>All Movies</h1>
 
+<div class="container">
+  <div class="row gy-4">
 @foreach($movies as $movie)
-<div class="card">
-  <img src="{{ $movie->image }}" class="card-img-top w-50" alt="{{ $movie->title }}">
-  <div class="card-body">
-    <h5 class="card-title">{{ $movie->title }}</h5>
-    <p class="card-text">{{ $movie->description }}</p>
-    <a href="{{ route('movies.show', $movie->id) }}" class="btn btn-primary">Read more</a>
+<div class="col-4">
+  <div class="card">
+    <img src="{{ $movie->image }}" class="card-img-top w-100" alt="{{ $movie->title }}">
+    <div class="card-body">
+      <h5 class="card-title">{{ $movie->title }}</h5>
+      <p class="card-text">{{ $movie->description }}</p>
+      <a href="{{ route('movies.show', $movie->id) }}" class="btn btn-primary">Read more</a>
+    </div>
   </div>
 </div>
-
 @endforeach
+</div>
+</div>
 
 @endsection
