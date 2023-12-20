0f8a9cdb48d279af1ebae4947b5d1b6a17107f53
RickCraco <riccardocracolici43@gmail.com>
2023-12-20 16:41:48 +0100

    aggiunti elementi di stile

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -6,6 +6,6 @@
 
 <h1 class="text-center">{{ $movie->title }}</h1>
 <div>
-    <img src="{{ $movie->image }}" alt="{{ $movie->title }}">
+    <img src="{{ $movie->image }}" alt="{{ $movie->title }}" class="w-50">
     <p>{{ $movie->description }}</p>
 </div>
