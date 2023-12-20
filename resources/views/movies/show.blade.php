990a066b5bef0ace10f61234b5e5041176160ddc
RickCraco <riccardocracolici43@gmail.com>
2023-12-20 16:47:35 +0100

    sistemato layout pagina

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -5,7 +5,9 @@
 @section('content')
 
 <h1 class="text-center">{{ $movie->title }}</h1>
-<div>
-    <img src="{{ $movie->image }}" alt="{{ $movie->title }}" class="w-50">
-    <p>{{ $movie->description }}</p>
+<div class="d-flex justify-content-center">
+    <img src="{{ $movie->image }}" alt="{{ $movie->title }}" class="w-25">
 </div>
+<h5 class="text-center">{{$movie->original_title}}</h5>
+
+@endsection
