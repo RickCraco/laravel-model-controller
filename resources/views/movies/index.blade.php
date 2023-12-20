f66cd73d8bffd4c55401b6b3f415d01c0a3e3747
RickCraco <riccardocracolici43@gmail.com>
2023-12-20 16:15:32 +0100

    aggiunto layout alla pagina index per i movies

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'All Movies')
+
+@section('content')
+
+<h1>All Movies</h1>
+
+@foreach($movies as $movie)
+<div class="card">
+  <img src="{{ $movie->image }}" class="card-img-top" alt="{{ $movie->title }}">
+  <div class="card-body">
+    <h5 class="card-title">{{ $movie->title }}</h5>
+    <p class="card-text">{{ $movie->description }}</p>
+    <a href="{{ route('movies.show', $movie->id) }}" class="btn btn-primary">Read more</a>
+  </div>
+</div>
